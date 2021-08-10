643b9a2105237d7d4aa4ed5caa7e271fc99d0f8d
y0ngma <takeit2sy@pusan.ac.kr>
2021-08-10 20:27:36 +0900

    작업 파일은 깃안에, 데이터는 깃밖에

---
+++ myfunctions.py
@@ -0,0 +1,88 @@
+import datetime, time
+import os, glob, zipfile
+import pickle
+import pandas as pd
+
+
+
+def zip_with_filecount_suffix( _src_path, dest_file ):
+    cnt = 0
+    print( '압축대상 {}\n압축이름 {}\n압축중...'.format(_src_path,dest_file) )
+    with zipfile.ZipFile( dest_file, 'w', zipfile.ZIP_DEFLATED ) as zf:
+        _rootpath = _src_path
+        for ( _dirpath, dirnames, filenames ) in os.walk( _src_path ):
+            for filename in filenames:
+                _target_path = os.path.join( _dirpath, filename )
+                cnt += 1
+                _rel_path = os.path.relpath( _target_path, _rootpath )
+                zf.write( _target_path, _rel_path )
+    print('압축 완료 !')
+    new_file_path = dest_file.split('.')[0]+'_{}EA.zip'.format(cnt)
+    try:
+        os.rename( dest_file, new_file_path )
+    except FileExistsError as e:
+        print('이름바꾸기 실패하여 pass함 ',e)
+        pass
+    
+
+
+def update_log_df(log_file, log_list_of_list):
+    '''
+    변환한 동영상 폴더별 파일갯수 파악하여 기존엑셀에 추가
+    log_file         : 로그 엑셀파일과 그 풀경로
+    log_list_of_list : 변환된 이미지폴더명과 그 안의 파일 갯수 등으로 이루어진 [[date,.., folder, count], [],[],..]
+    '''
+    if not os.path.exists(log_file): # 최초실행시 작동
+        df_old = pd.DataFrame()
+        print('로그파일이 존재하지 않음', log_file)
+        df_old.to_excel(log_file)
+    df_old = pd.read_excel(log_file, index_col=0)
+    df_to_append = pd.DataFrame(log_list_of_list, columns=['converted_date','converted_time','basename','folder_name','file_count'])
+    print(df_to_append)
+    df_new = pd.concat([df_old,df_to_append])
+    df_new.to_excel(log_file)
+
+
+
+def aimmo_xlsx(log_file, target_path:str):
+    """
+    aimmo제출용 "idx", "체널2", "체널1", "체널2파일수", "체널1파일수"를 컬럼으로 갖는 엑셀만들기 
+    """
+    def idx_from_1(df:pd.DataFrame):
+        # df의 인덱스가 1부터 시작하게끔 하는 방법 2가지
+        if df.index[0]==0:
+            df.index = df.index + 1
+        else:
+            import numpy as np
+            df.index = np.arange(1,len(df)+1)
+
+    # 한 폴더내에 채널별로 저장된 폴더명(*_ch1, *_ch2)과 해당 폴더내 파일수 측정
+    folder_ch1 = [ path.split("\\")[-1] for path in glob.glob(target_path+"\\*ch1") ]
+    folder_ch2 = [ path.split("\\")[-1] for path in glob.glob(target_path+"\\*ch2") ]
+    ch1_cnt = [ len(os.listdir(path)) for path in glob.glob(target_path+"\\*ch1") ]
+    ch2_cnt = [ len(os.listdir(path)) for path in glob.glob(target_path+"\\*ch1") ]
+    both_channel = list(zip(folder_ch2, folder_ch1, ch2_cnt, ch1_cnt))
+    # print(*both_channel, sep='\n')
+    cols = ['left', 'right', 'ch2_cnt', 'ch1_cnt']
+    to_append  = pd.DataFrame(both_channel, columns=cols)
+    bookmarker = pd.DataFrame([["+"*28, "+"*28, "+"*7, "+"*7]], columns=cols)
+    idx_from_1(to_append)
+    print(to_append)
+    # 기존 로그 엑셀파일 읽어들여서 업데이트 하기        
+    if not os.path.exists(log_file):
+        df_old = pd.DataFrame()
+        df_old.to_excel(log_file)
+    df_old = pd.read_excel(log_file, index_col=0)
+    df_old = pd.concat([df_old, to_append])
+    df_old = pd.concat([df_old, bookmarker])
+    df_old.to_excel(log_file)
+        
+
+
+if __name__ == "__main__":
+    PROJECT_DIR      = os.path.dirname( os.path.abspath(__file__) )+"\\"
+    log_path         = PROJECT_DIR+'log_folder\\' # 로그보관용 폴더경로
+    base_path        = "E:\\aimmo"
+    target_path_list = glob.glob(base_path+'\\*_image')
+    
+    aimmo_xlsx(log_path+"log_aimmo.xlsx", target_path_list)
---
+++ nas_down.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import glob, os, sys, shutil
+import time, datetime
+# import numpy as np
+import pandas as pd
+import cv2
+
+# archive_path = "//192.168.0.12/homes/brooks/DONJJANG"
+archive_path = "/home/qtumai/jason/nas"
+
+DOWNLOAD_DIR = '/home/qtumai/jason/computervision'
+# DOWNLOAD_DIR = 'C:/Users/home/qtumai/jason/computervision/SHARED_FOLDER'
+
+target_shop = 'DONJJANG'
+target_dates = [
+# "20210804",
+"20210805",
+"20210806",
+"20210807",
+"20210808",
+]
+hour_list      = [ '00','01','02','03','04','05','06','07','08','09','10','11',
+                   '12','13','14','15','16','17','18','19','20','21','22','23' ]
+target_hours   = hour_list[12:23]
+
+all_file_list = [ file for file in glob.glob(archive_path +"/*.avi") if os.path.getsize(file)>900000000 ]
+
+target_path_list = list()
+file_size_list   = list()
+duration_list    = list()
+# 전체 파일리스트에서 파일명의 날짜와 상호명으로 원하는것만 작업
+for file_path in all_file_list:
+    filename         = os.path.basename(file_path)
+    video_name       = os.path.splitext( filename )[0]
+    video_datetime   = video_name.split('_')[0]
+    video_shopname   = video_name.split('_')[1]
+    if video_shopname != target_shop: continue
+    else:
+        video_date       = video_datetime[:8]
+        for target_date in target_dates:
+            if video_date != target_date: continue
+            else:
+                # 업소별 특징 적용
+                if video_shopname == "DONJJANG": # 어두운 거리 특성상 오전포함, 야간제외
+                    target_hours = hour_list[8:20]
+                
+                for target_hour in target_hours:
+                    video_hour = video_datetime[8:10]
+                    if video_hour == target_hour:
+                        # 업소별 특징 적용
+                        video_camnumber  = video_name.split('_')[2]
+                        if video_shopname == "SW365": # 업체 요청으로 좌우 채널명 교환
+                            if video_name.split('_')[2] == "ch1":
+                                video_camnumber = "ch2"
+                            if video_name.split('_')[2] == "ch2":
+                                video_camnumber = "ch1"
+
+                        video_minute     = video_datetime[10:12]
+                        video_second     = video_datetime[12:video_name.find('_')]
+                        str_HHMMSS       = video_hour +":"+ video_minute +":"+ video_second
+                        str_HHMMSS       = datetime.datetime.strptime(str_HHMMSS, "%H:%M:%S")
+                        video_time       = video_hour + video_minute + video_second
+                        date_time_prefix = video_date+'_'+video_time
+                        # 비디오 길이측정
+                        cap = cv2.VideoCapture(file_path)
+                        cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+                        fps = cap.get(cv2.CAP_PROP_FPS)
+                        cap.release()
+                        if fps == 0: continue
+                        else:
+                            seconds  = int(cnt/fps)
+                            duration = datetime.timedelta(seconds=seconds)
+                            min      = int( str(duration).split(":")[-2] )
+                            # 목표파일, 파일용량, 비디오길이 리스트 생성
+                            duration_list.append(min)
+                            target_path_list.append(file_path)
+                            file_size_list.append( os.path.getsize(file_path) )
+
+zipped_lists = list( zip(target_path_list, file_size_list, duration_list,) )
+df = pd.DataFrame( zipped_lists, columns=['path', 'size', 'duration'] )
+print(df.head())
+print(df.groupby(by=['duration']).count())
+
+workable_list = df[df['duration']>=24]['path'].to_list()
+print( '사용가능 파일 갯수= {}, 비율= {}%'.format(
+    len(workable_list), int(100*len(workable_list)/len(target_path_list))) )
+
+
+
+# 파일을 로컬로 이동
+for workable in workable_list:
+    filename = os.path.basename(workable)
+    print(workable)
+    print(DOWNLOAD_DIR +"/"+ filename)
+    shutil.copy(workable, DOWNLOAD_DIR +"/"+ filename)
+
+
+# 변환 작업
+
+
+
+# # 영상길이가 24분 이하일 경우 1000장보다 적게 이미지변환 될 수 있음
+# df23 = df[df['duration']< 24]
+# move_file_list = df[df['duration']<24]['path'].to_list()
+# move_dst_path = archive_path+"/small_file"
+# if not os.path.isdir(archive_path+"/small_file"):
+#     os.mkdir(move_dst_path)
+# print('경로생성')
+# for move_file in move_file_list:
+#     file_name = move_file.split("/")[-1]
+#     shutil.move(move_file, move_dst_path+"/"+file_name)
+
+
+
+# plt.hist(file_size_list, color='c', bins=10 )
+# plt.xlabel('file_size')
+# plt.ylabel('frequency')
+# plt.title('histogram')
+# plt.grid()
+# plt.show()
+
+# small_size = np.quantile(file_size_list, 0.9)
+# df1 = df[ (df['size'] < df['size'].quantile(0.5)) & (df['size'] > df['size'].quantile(0.3)) ]
+# df2 = df[ df['size']<df['size'].quantile(0.1) ]
+# df3 = df[ df['size']>df['size'].quantile(0.99) ]
+
+# for idx in range(df2.shape[0]):
+#     file_path = df2.iloc[idx,0]
+#     file_size = df2.iloc[idx,1]
+#     cap = cv2.VideoCapture(file_path)
+#     cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+#     fps = cap.get(cv2.CAP_PROP_FPS)
+#     seconds = int(cnt/fps)
+#     duration = datetime.timedelta(seconds=seconds)
+#     print( "경로{:<34}, 크기= {:_>10}, 길이= {}".format(file_path, file_size, duration) )
+    
+# mod = sys.modules[__name__]
+# for idx in range(1,11):
+#     setattr(mod, 'quantile0{}'.format(idx), idx)
+
+# for idx in range(1,11):
+#     globals()[ 'quantile0{}'.format(idx) ]=1
+
+# print(quantile02)
+
