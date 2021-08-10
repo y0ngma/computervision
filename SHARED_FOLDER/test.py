31394bce13727dfce2c506ba5b4447d653680456
y0ngma <takeit2sy@pusan.ac.kr>
2021-08-10 16:03:13 +0900

    테스트

--- SHARED_FOLDER/test.py
+++ SHARED_FOLDER/test.py
@@ -7,12 +7,13 @@ import pandas as pd
 # import matplotlib.pyplot as plt
 import cv2
 
-
-# archive_path = "\\\\192.168.0.12\\homes\\Jason\\test_video"
-# archive_path = "\\\\192.168.0.12\\homes\\brooks\\DONJJANG"
+# archive_path = "//192.168.0.12/homes/Jason/test_video"
+# archive_path = "//192.168.0.12/homes/brooks/DONJJANG"
 archive_path = "/home/qtumai/jason/nas"
-MOVE_PATH = 'D:\\'
-# MOVE_PATH = 'C:\\Users\\home\\qtumai\\jason\\computervision\\SHARED_FOLDER'
+archive_path = "D:/새 폴더"
+
+MOVE_PATH = 'D:/'
+# MOVE_PATH = 'C:/Users/home/qtumai/jason/computervision/SHARED_FOLDER'
 
 target_shop = 'DONJJANG'
 target_date = [
@@ -22,61 +23,91 @@ target_date = [
 "20210807",
 "20210808",
 ]
-all_file_list = glob.glob(archive_path +"\\*.avi")
-file_size_list = [ os.path.getsize(file_path) for file_path in all_file_list ]
-duration_list = list()
 
+all_file_list = [ file for file in glob.glob(archive_path +"/*.avi") if os.path.getsize(file)>900000000 ]
+
+target_path_list = list()
+file_size_list   = list()
+duration_list    = list()
+# 전체 파일리스트에서 파일명의 날짜와 상호명으로 원하는것만 작업
 for file_path in all_file_list:
-    cap = cv2.VideoCapture(file_path)
-    cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    seconds = int(cnt/fps)
-    duration = datetime.timedelta(seconds=seconds)
-    min = int( str(duration).split(":")[-2] )
-    duration_list.append(min)
-    cap.release()
+    filename         = os.path.basename(file_path)
+    video_name       = os.path.splitext( filename )[0]
+    video_datetime   = video_name.split('_')[0]
+    video_shopname   = video_name.split('_')[1]
+    if video_shopname != target_shop: continue
+    else:
+        video_camnumber  = video_name.split('_')[2]
+        if video_shopname == "SW365": # 업체 요청으로 좌우 채널명 교환
+            if video_name.split('_')[2] == "ch1":
+                video_camnumber = "ch2"
+            if video_name.split('_')[2] == "ch2":
+                video_camnumber = "ch1"
+        video_date       = video_datetime[:8]
+        for date in target_date:
+            if video_date != date: continue
+            else:
+                video_hour       = video_datetime[8:10]
+                video_minute     = video_datetime[10:12]
+                video_second     = video_datetime[12:video_name.find('_')]
+                str_HHMMSS       = video_hour +":"+ video_minute +":"+ video_second
+                str_HHMMSS       = datetime.datetime.strptime(str_HHMMSS, "%H:%M:%S")
+                video_time       = video_hour + video_minute + video_second
+                date_time_prefix = video_date+'_'+video_time
+                # 비디오 길이측정
+                cap = cv2.VideoCapture(file_path)
+                cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+                fps = cap.get(cv2.CAP_PROP_FPS)
+                cap.release()
+                if fps == 0: continue
+                else:
+                    seconds  = int(cnt/fps)
+                    duration = datetime.timedelta(seconds=seconds)
+                    min      = int( str(duration).split(":")[-2] )
+                    # 목표파일, 파일용량, 비디오길이 리스트 생성
+                    duration_list.append(min)
+                    target_path_list.append(file_path)
+                    file_size_list.append( os.path.getsize(file_path) )
 
-zipped_lists = list( zip(all_file_list, file_size_list, duration_list,) )
-# print(zipped_lists)
+zipped_lists = list( zip(target_path_list, file_size_list, duration_list,) )
 df = pd.DataFrame( zipped_lists, columns=['path', 'size', 'duration'] )
 print(df.head())
 print(df.groupby(by=['duration']).count())
 
 workable_list = df[df['duration']>=24]['path'].to_list()
 print( '사용가능 파일 갯수= {}, 비율= {}%'.format(
-    len(workable_list), int(100*len(workable_list)/len(all_file_list))) )
+    len(workable_list), int(100*len(workable_list)/len(target_path_list))) )
 
-# 파일명에서 이름이나 시간으로 필터링
-for workable in workable_list:
-    filename         = os.path.basename(workable)
-    video_name       = os.path.splitext( filename )[0]
-    video_datetime   = video_name.split('_')[0]
-    video_shopname   = video_name.split('_')[1]
-    video_camnumber  = video_name.split('_')[2]
-    if video_shopname == "SW365": # 업체 요청으로 좌우 채널명 교환
-        if video_name.split('_')[2] == "ch1":
-            video_camnumber = "ch2"
-        if video_name.split('_')[2] == "ch2":
-            video_camnumber = "ch1"
-    video_date       = video_datetime[:8]
-    video_hour       = video_datetime[8:10]
-    video_minute     = video_datetime[10:12]
-    video_second     = video_datetime[12:video_name.find('_')]
-    str_HHMMSS       = video_hour +":"+ video_minute +":"+ video_second
-    str_HHMMSS       = datetime.datetime.strptime(str_HHMMSS, "%H:%M:%S")
-    video_time       = video_hour + video_minute + video_second
-    date_time_prefix = video_date+'_'+video_time
+# for workable in workable_list:
+#     filename         = os.path.basename(workable)
+#     video_name       = os.path.splitext( filename )[0]
+#     video_datetime   = video_name.split('_')[0]
+#     video_shopname   = video_name.split('_')[1]
+#     video_camnumber  = video_name.split('_')[2]
+#     if video_shopname == "SW365": # 업체 요청으로 좌우 채널명 교환
+#         if video_name.split('_')[2] == "ch1":
+#             video_camnumber = "ch2"
+#         if video_name.split('_')[2] == "ch2":
+#             video_camnumber = "ch1"
+#     video_date       = video_datetime[:8]
+#     video_hour       = video_datetime[8:10]
+#     video_minute     = video_datetime[10:12]
+#     video_second     = video_datetime[12:video_name.find('_')]
+#     str_HHMMSS       = video_hour +":"+ video_minute +":"+ video_second
+#     str_HHMMSS       = datetime.datetime.strptime(str_HHMMSS, "%H:%M:%S")
+#     video_time       = video_hour + video_minute + video_second
+#     date_time_prefix = video_date+'_'+video_time
     
-    # 파일을 로컬로 이동
-    if video_shopname == target_shop:
-        for date in target_date:
-            if video_date == date:
-                print(workable)
-                print(MOVE_PATH +"\\"+ filename)
-                # shutil.move(workable, MOVE_PATH +"\\"+ filename)
-    else:
-        # print('본파일의 {}는 원하는 상호명이 아니므로 넘어감'.format(video_shopname))
-        pass
+#     # 파일을 로컬로 이동
+#     if video_shopname == target_shop:
+#         for date in target_date:
+#             if video_date == date:
+#                 print(workable)
+#                 print(MOVE_PATH +"\\"+ filename)
+#                 # shutil.move(workable, MOVE_PATH +"\\"+ filename)
+#     else:
+#         # print('본파일의 {}는 원하는 상호명이 아니므로 넘어감'.format(video_shopname))
+#         pass
 
 
 # 변환 작업
