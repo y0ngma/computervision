b232c33058206c06394134ff290ced8f260773be
y0ngma <takeit2sy@pusan.ac.kr>
2021-08-11 14:35:34 +0900

    경로 끝에 슬래쉬 붙이지 않기

--- nas_down.py
+++ nas_down.py
@@ -27,81 +27,84 @@ hour_list      = [ '00','01','02','03','04','05','06','07','08','09','10','11',
                    '12','13','14','15','16','17','18','19','20','21','22','23' ]
 target_hours   = hour_list[12:23]
 
-all_file_list = [ file for file in glob.glob(NAS_DIR +"/*.avi") if os.path.getsize(file)>920000000 ]
 
-target_path_list = list()
-file_size_list   = list()
-duration_list    = list()
-# 전체 파일리스트에서 파일명의 날짜와 상호명으로 원하는것만 작업
-for file_path in all_file_list:
-    filename         = os.path.basename(file_path)
-    video_name       = os.path.splitext( filename )[0]
-    video_datetime   = video_name.split('_')[0]
-    video_shopname   = video_name.split('_')[1]
-    if video_shopname != target_shop: continue
-    else:
-        video_date = video_datetime[:8]
-        video_hour = video_datetime[8:10]
-        for target_date in target_dates:
-            if video_date != target_date: continue
-            else:
-                # 업소별 특징 적용 1. 어두운 거리 특성상 오전포함, 야간제외
-                if video_shopname == "DONJJANG":
-                    target_hours = hour_list[11:20]
-                video_camnumber  = video_name.split('_')[2]
-                # 업소별 특징 적용 2.업체 요청으로 좌우 채널명 교환
-                if video_shopname == "SW365":
-                    if video_name.split('_')[2] == "ch1":
-                        video_camnumber = "ch2"
-                    if video_name.split('_')[2] == "ch2":
-                        video_camnumber = "ch1"
+
+# all_file_list    = [ file for file in glob.glob(NAS_DIR +"/*.avi") if os.path.getsize(file)>920000000 ]
+# print('조회된 파일 수 :', len(all_file_list))
+# target_path_list = list()
+# file_size_list   = list()
+# duration_list    = list()
+# # 전체 파일리스트에서 파일명의 날짜와 상호명으로 원하는것만 작업
+# for file_path in all_file_list:
+#     filename         = os.path.basename(file_path)
+#     video_name       = os.path.splitext( filename )[0]
+#     video_datetime   = video_name.split('_')[0]
+#     video_shopname   = video_name.split('_')[1]
+#     if video_shopname != target_shop: continue
+#     else:
+#         video_date = video_datetime[:8]
+#         video_hour = video_datetime[8:10]
+#         for target_date in target_dates:
+#             if video_date != target_date: continue
+#             else:
+#                 # 업소별 특징 적용 1. 어두운 거리 특성상 오전포함, 야간제외
+#                 if video_shopname == "DONJJANG":
+#                     target_hours = hour_list[8:20]
+#                 video_camnumber  = video_name.split('_')[2]
+#                 # 업소별 특징 적용 2.업체 요청으로 좌우 채널명 교환
+#                 if video_shopname == "SW365":
+#                     if video_name.split('_')[2] == "ch1":
+#                         video_camnumber = "ch2"
+#                     if video_name.split('_')[2] == "ch2":
+#                         video_camnumber = "ch1"
                 
-                for target_hour in target_hours:
-                    if video_hour == target_hour:
-                        # 비디오 길이측정
-                        cap = cv2.VideoCapture(file_path)
-                        cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-                        fps = cap.get(cv2.CAP_PROP_FPS)
-                        cap.release()
-                        if fps == 0: continue
-                        else:
-                            seconds  = int(cnt/fps)
-                            duration = datetime.timedelta(seconds=seconds)
-                            min      = int( str(duration).split(":")[-2] )
-                            # 목표파일, 파일용량, 비디오길이 리스트 생성
-                            duration_list.append(min)
-                            target_path_list.append(file_path)
-                            file_size_list.append( os.path.getsize(file_path) )
+#                 for target_hour in target_hours:
+#                     if video_hour == target_hour:
+#                         # 비디오 길이측정
+#                         print('영상길이측정중... : ', file_path)
+#                         cap = cv2.VideoCapture(file_path)
+#                         cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+#                         fps = cap.get(cv2.CAP_PROP_FPS)
+#                         cap.release()
+#                         if fps == 0: continue
+#                         else:
+#                             seconds  = int(cnt/fps)
+#                             duration = datetime.timedelta(seconds=seconds)
+#                             min      = int( str(duration).split(":")[-2] )
+#                             # 목표파일, 파일용량, 비디오길이 리스트 생성
+#                             duration_list.append(min)
+#                             target_path_list.append(file_path)
+#                             file_size_list.append( os.path.getsize(file_path) )
 
-zipped_lists = list( zip(target_path_list, file_size_list, duration_list,) )
-df = pd.DataFrame( zipped_lists, columns=['path', 'size', 'duration'] )
-print(df.head())
-print(df.groupby(by=['duration']).count())
+# zipped_lists = list( zip(target_path_list, file_size_list, duration_list,) )
+# df = pd.DataFrame( zipped_lists, columns=['path', 'size', 'duration'] )
+# print(df.head())
+# print(df.groupby(by=['duration']).count())
 
-workable_list = df[df['duration']>=24]['path'].to_list()
-print( '사용가능 파일 갯수= {}, 비율= {}%'.format(
-    len(workable_list), int(100*len(workable_list)/len(target_path_list))) )
-
-
-
-# 파일을 로컬로 이동
-for workable in workable_list[-2:]:
-    filename = os.path.basename(workable)
-    print('나스->로컬경로 :', DOWNLOAD_DIR +"/"+ filename, 'copyfile 하는중...')
-    shutil.copyfile(workable, DOWNLOAD_DIR +"/"+ filename)
-print('이동완료')
+# workable_list = df[df['duration']>=24]['path'].to_list()
+# print( '사용가능 파일 갯수= {}, 비율= {}%'.format(
+#     len(workable_list), int(100*len(workable_list)/len(target_path_list))) )
 
 
 
+# # 파일을 로컬로 이동
+# for workable in workable_list:
+#     filename = os.path.basename(workable)
+#     print('나스->로컬경로 :', DOWNLOAD_DIR +"/"+ filename, 'copyfile 하는중...')
+#     shutil.copyfile(workable, DOWNLOAD_DIR +"/"+ filename)
+# print('이동완료')
+
+
+#####################################################################################
 # 변환 작업
 original_fps     = 30 # 변환할 원본영상의 fps
 fps_list         = 30 # 영상에서 가져올 프레임 간격
 log_path         = archive_path # 로그보관용 폴더경로
 unplayable_video = list()
 worked_dirs      = list()
-for file in glob.glob( DOWNLOAD_DIR+'*.avi' ):
+for file in glob.glob( DOWNLOAD_DIR+'/*.avi' ):
     FILE_TO_WORK     = True
-    video_name       = os.path.splitext(file)[0].split('\\')[-1]
+    video_name       = os.path.splitext(file)[0].split('/')[-1]
     if video_name[:4] == '2021' and video_name[14] == '_':
         video_datetime   = video_name.split('_')[0]
         video_shopname   = video_name.split('_')[1]
@@ -126,14 +129,14 @@ for file in glob.glob( DOWNLOAD_DIR+'*.avi' ):
 
         # 작업한 원본영상 옮겨놓을 경로 
         FPS           = '_' + str(1 + original_fps - fps_list) + 'FPS'
-        original_path = archive_path + basename +'_ori\\'
+        original_path = archive_path +'/'+ basename +'_ori/'
         if not os.path.isdir(original_path):
             os.makedirs(original_path, exist_ok=True)
             print('생성된 경로', original_path)
 
         # 1000번째까지의 작업한 사진 저장경로(업체요청)
-        worked_dirs.append(archive_path + basename +"_image\\")
-        save_path = archive_path + basename +"_image\\"+ new_video_name +'\\'
+        worked_dirs.append(archive_path +"/"+ basename +"_image")
+        save_path = archive_path +"/"+ basename +"_image/"+ new_video_name +'/'
         if not os.path.isdir(save_path):
             os.makedirs(save_path, exist_ok=True)
             print('생성된 경로', save_path)
@@ -141,7 +144,7 @@ for file in glob.glob( DOWNLOAD_DIR+'*.avi' ):
         # 파일명에서 120000~235959에 해당하는것만 변환
         target_hours = hour_list[12:24]
         if video_shopname == "DONJJANG": # 특성상 오전포함, 야간제외
-            target_hours = hour_list[11:20]
+            target_hours = hour_list[7:20]
 
         for target_hour in target_hours:
             flag = False
@@ -209,15 +212,20 @@ for file in glob.glob( DOWNLOAD_DIR+'*.avi' ):
                 print("Save %d images//work_time %s" % (cnt, work_time))
                 
                 # 작업이 끝난 원본파일을 따로 옮겨서 정리
-                shutil.move( file, os.path.join(original_path, file.split('\\')[-1]) )
-                print( '여기서 {} -->\n여기로 {}'.format(file, os.path.join(original_path, file.split('\\')[-1])) )
+                shutil.move( file, os.path.join(original_path, file.split('/')[-1]) )
+                print( '여기서 {} -->\n여기로 {}'.format(file, os.path.join(original_path, file.split('/')[-1])) )
                 
 
 print( "문제가 생긴 영상 : ", *unplayable_video, sep='\n' )
 for worked_dir in worked_dirs:
     if os.path.isdir(worked_dir):
-        aimmo_xlsx(log_path+"log_aimmo.xlsx", worked_dir) # 에이모제공용 엑셀 업데이트
+        print(worked_dir)
+        aimmo_xlsx(log_path+"/log_aimmo.xlsx", worked_dir) # 에이모제공용 엑셀 업데이트
         # zip_with_filecount_suffix(worked_dir, worked_dir+'.zip') # 압축하기
+
+##########################################################################################################
+
+
 
 
 
